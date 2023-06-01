d5ae6e15b2618f36585e2f79d67a62ec309aaa9d
ruhulamin87 <ruhul4159@gmail.com>
2023-06-01 20:03:25 +0300

    Final

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const bookRoutes = require('./bookRoutes');
-
-const app = express();
-const port = 3000;
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/bookstore', {
@@ -16,10 +11,3 @@ mongoose.connect('mongodb://localhost/bookstore', {
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
-
-app.use(express.json());
-app.use('/', bookRoutes);
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
